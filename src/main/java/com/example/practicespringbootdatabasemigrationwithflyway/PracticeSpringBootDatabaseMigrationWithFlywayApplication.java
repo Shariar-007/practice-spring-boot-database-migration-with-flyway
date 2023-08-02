d73758ded11252115db08e854492294be2a79b14
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2023-08-02 16:33:16 +0600

    first commit

---
+++ src/main/java/com/example/practicespringbootdatabasemigrationwithflyway/PracticeSpringBootDatabaseMigrationWithFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicespringbootdatabasemigrationwithflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootDatabaseMigrationWithFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringBootDatabaseMigrationWithFlywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practicespringbootdatabasemigrationwithflyway/PracticeSpringBootDatabaseMigrationWithFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicespringbootdatabasemigrationwithflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringBootDatabaseMigrationWithFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
